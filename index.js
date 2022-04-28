d10a1a53969a5c8d3d4c11d3d400b66ad2e0984f
yusuke082 <85321023+yusuke082@users.noreply.github.com>
2022-04-28 18:41:25 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,4 +7,7 @@ window.onload = async function(){
   const provider = await new ethers.providers.Web3Provider(window.ethereum);
   const blocknumber = await provider.getBlockNumber();
   alert(blocknumber);
+  const addresses = await ethereum.request({method:'eth_requestAccounts'});
+  const signer = await provider.getSigner();
+  alert(addresses[0]);
 }
