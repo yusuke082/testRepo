175102c01616565eaea5e07d468f64cb82561222
yusuke082 <85321023+yusuke082@users.noreply.github.com>
2022-04-28 18:30:55 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 console.log("hellow world");
 
 window.onload = async function(){
-  const provider = await new ethers.providers.Web3Provider(window.ethereum);
+  const provider = await new ethers.providers.Web3Provider(window.ethereum)
   const blocknumber = await provider.getBlockNumber();
   alert(blocknumber);
-}();
+}
